ba57812038ec2b61c6a412ad419db6e45c447faa
David Vickery <david.vickery.23@gmail.com>
2015-02-26 19:45:13 +0000

    switch to postresql

--- config/database.rb
+++ config/database.rb
@@ -4,10 +4,24 @@ configure do
     ActiveRecord::Base.logger = Logger.new(STDOUT)
   end
 
-  set :database, {
-    adapter: "sqlite3",
-    database: "db/db.sqlite3"
-  }
+  credentials = {   adapter: 'postgresql',
+    encoding: 'unicode',
+    pool: 5,
+    database: 'dc8f0lcnp9kvi4',
+    username: 'ootypuxurllzbv',
+    password: 'nXkVSWjV9jsKMzX74HUDJHns15',
+    host: 'ec2-54-235-127-33.compute-1.amazonaws.com',
+    port: 5432,
+    min_messages: 'error' }
+
+  puts "Establishing connection to database ..."
+  ActiveRecord::Base.establish_connection( credentials )
+  puts "CONNECTED"
+
+  # set :database, {
+  #   adapter: "sqlite3",
+  #   database: "db/db.sqlite3"
+  # }
 
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
